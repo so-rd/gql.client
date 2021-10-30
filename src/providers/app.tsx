7ed187e66afa2d5b595acb1e04f40235b963cdef
Mason Smith <mason.smith@solutionops.com>
2021-10-30 12:00:02 -0600

    update notifications #4

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -61,18 +61,18 @@ export const AppProvider = ({ children }: AppProviderProps) => {
                 colorScheme,
                 colors: {
                   // override dark colors here to change them for all components
-                  dark: [
-                    '#d5d7e0',
-                    '#acaebf',
-                    '#8c8fa3',
-                    '#666980',
-                    '#4d4f66',
-                    '#34354a',
-                    '#2b2c3d',
-                    '#1d1e30',
-                    '#0c0d21',
-                    '#01010a',
-                  ],
+                  // dark: [
+                  //   '#d5d7e0',
+                  //   '#acaebf',
+                  //   '#8c8fa3',
+                  //   '#666980',
+                  //   '#4d4f66',
+                  //   '#34354a',
+                  //   '#2b2c3d',
+                  //   '#1d1e30',
+                  //   '#0c0d21',
+                  //   '#01010a',
+                  // ],
                 },
               }}
             >
